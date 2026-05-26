6ad678d21b1c28782b25699de8294793683cc417
Steef0712 <steefdegraaff2004@gmail.com>
2026-05-26 11:14:12 +0200

    Bars

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,39 +6,32 @@ public class Player : MonoBehaviour
     public HealthBar hungerBar;
     public HealthBar energyBar;
 
+    [Header("Drain Rates (per second)")]
+    public float hungerDrainRate = 5f;
+    public float energyDrainRate = 10f;
+
     void Update()
     {
         // --- HUNGER ---
-        float newHunger = hungerBar.currentValue - (5f * Time.deltaTime);
+        float newHunger = hungerBar.currentValue - (hungerDrainRate * Time.deltaTime);
         hungerBar.SetValue(newHunger);
 
         if (Input.GetMouseButtonDown(0))
-        {
             hungerBar.SetValue(hungerBar.currentValue - 10f);
-        }
 
         if (Input.GetMouseButtonDown(1))
-        {
             hungerBar.SetValue(hungerBar.maxValue);
-        }
 
         // --- ENERGY ---
-        float newEnergy = energyBar.currentValue - (10f * Time.deltaTime);
+        float newEnergy = energyBar.currentValue - (energyDrainRate * Time.deltaTime);
         energyBar.SetValue(newEnergy);
 
         if (Input.GetMouseButtonDown(2))
-        {
             energyBar.SetValue(energyBar.maxValue);
-        }
 
         // --- SCENE SWITCHING ---
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-            SceneManager.LoadScene("Scene1");
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-            SceneManager.LoadScene("Scene2");
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-            SceneManager.LoadScene("Scene3");
+        if (Input.GetKeyDown(KeyCode.Alpha1)) SceneManager.LoadScene("Scene1");
+        if (Input.GetKeyDown(KeyCode.Alpha2)) SceneManager.LoadScene("Scene2");
+        if (Input.GetKeyDown(KeyCode.Alpha3)) SceneManager.LoadScene("Scene3");
     }
 }
