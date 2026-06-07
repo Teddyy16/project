69d92b63354105b875140599491688e7224dbee4
AsmartNerv <steinsmikuss@gmail.com>
2026-06-07 23:12:54 +0200

    Added background music

---
+++ Assets/Music.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+
+    public static Music instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
